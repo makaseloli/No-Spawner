dff2688bbea1cf70db27ba22049a3b6c150f3e1f
Administrator <114403771+makaseloli@users.noreply.github.com>
2025-08-10 22:30:11 +0900

    Add support for 1.21.1

---
+++ src/main/java/io/github/makaseloli/nospawner/Nospawner.java
@@ -0,0 +1,8 @@
+package io.github.makaseloli.nospawner;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Nospawner.MODID)
+public class Nospawner {
+    public static final String MODID = "nospawner";
+}
